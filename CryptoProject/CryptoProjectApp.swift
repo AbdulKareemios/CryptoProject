5c57d9450f90fe4ba093222cbf7168d912a13166
AbdulKareemios <a.kareem.tn@gmail.com>
2024-05-04 12:37:11 +0500

    Initial Commit

---
+++ CryptoProject/CryptoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoProjectApp.swift
+//  CryptoProject
+//
+//  Created by AK on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
